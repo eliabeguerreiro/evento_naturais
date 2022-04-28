e23cf2efb5ecb5028b66700beb86c024728f416a
eliabe <eliabepaz.work@gmail.com>
2022-04-28 12:12:05 -0300

    pdf ingresso

--- system_mail/sendEmail.php
+++ system_mail/sendEmail.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     include("./functions/conection.php");
-    
+
     var_dump($_SESSION);
     $part = "SELECT * FROM participantes WHERE ID = ".$_GET['id']."";
     $participante = mysqli_query($conn, $part);
@@ -115,7 +115,16 @@
         <h1>1º Simpósio Naturais</h1>
     </div>
     <div class="Grupo2">
-        <p>Olá, <?php echo($_SESSION['participante']['NOME']) ?>. Sua participação no primeiro simpósio naturais está confirmado(a). Seu ingresso é <?php echo($_SESSION['comprovante']['codigo']) ?>.</p>
+        <p>Olá, <?php echo($_SESSION['participante']['NOME']) ?>. Sua participação no primeiro simpósio naturais está confirmado(a). Seu ingresso é <?php 
+        $comp = "SELECT * FROM entradas WHERE id_cliente = ".$_GET['id']."";
+        $comprovante = mysqli_query($conn, $comp);
+       
+        while($_SESSION['comprovante'] = mysqli_fetch_assoc($comprovante)){
+    
+          echo$_SESSION['comprovante']['codigo'];
+    
+        }
+        ?>.</p>
     </div>
     <div class="Grupo3">
         <p>att. Equipe Redepharma.</p>
